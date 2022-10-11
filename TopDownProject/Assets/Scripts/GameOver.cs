028ceb51aa9585fa7fb3d4f703ba744f54605285
Asuran Delarosa <115186200+Asupapas@users.noreply.github.com>
2022-10-11 13:57:56 -0700

    Made Game over screen

--- TopDownProject/Assets/Scripts/GameOver.cs
+++ TopDownProject/Assets/Scripts/GameOver.cs
@@ -19,7 +19,7 @@ public class GameOver : MonoBehaviour
     public void TryAgain()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene("Forest");
+        SceneManager.LoadScene("Level1");
     }
 
 }
