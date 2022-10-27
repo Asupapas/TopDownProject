533d69210368016ec8edb162a143059bc327ab89
Asuran Delarosa <115186200+Asupapas@users.noreply.github.com>
2022-10-27 11:49:09 -0700

    joemama

--- TopDownProject/Assets/Scripts/AttackArea.cs
+++ TopDownProject/Assets/Scripts/AttackArea.cs
@@ -8,7 +8,11 @@ public class AttackArea : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider.GetComponent<EnemyHealthNew>() != null)
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        GetComponent<Animator>().SetFloat("xInput", xInput);
+        GetComponent<Animator>().SetFloat("yInput", yInput);
+        if (collider.GetComponent<EnemyHealthNew>() != null)
         {
             EnemyHealthNew health = collider.GetComponent<EnemyHealthNew>();
             health.Damage(damage);
